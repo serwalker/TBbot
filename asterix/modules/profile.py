ea066f0d17c2b9a16baf406fdaf634eef615f8bc
TheHackerCatX <TheHackerCatX@users.noreply.github.com>
2022-08-18 11:52:09 +0000

    pylint: auto fixes

--- asterix/modules/profile.py
+++ asterix/modules/profile.py
@@ -401,5 +401,6 @@ async def rmprofile(m: Message, args):
 @app.on_message(gen("repo", allow=["sudo"]))
 async def repolink_handler(_, m: Message):
     await app.send_edit(
-        m, "[Here Is Asterix Userbot Repo](https://github.com/TigerNetwork/Tiger-USERBOT)"
+        m,
+        "[Here Is Asterix Userbot Repo](https://github.com/TigerNetwork/Tiger-USERBOT)",
     )
